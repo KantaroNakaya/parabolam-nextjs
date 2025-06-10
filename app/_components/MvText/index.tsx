11a94a45655a2c26f1d04844f5fc65a12c2ac64c
KantaroNakaya <shimeno38saiko@gmail.com>
2025-06-10 14:54:33 +0900

    メインビジュアルのデザイン変更

--- app/_components/MvText/index.tsx
+++ app/_components/MvText/index.tsx
@@ -1,15 +1,11 @@
-import Image from "next/image";
 import styles from "./index.module.css";
 export default function MvText() {
   return (
-    <div className={styles.mvText}>
-      <Image
-        src="/mvText.png"
-        alt="parabolam"
-        loading="lazy"
-        width={500}
-        height={500}
-      />
+    <div className={styles.content}>
+      <p className={styles.text}>
+        <span className={styles.text1}>心斎橋駅前バー</span>
+        <span className={styles.text2}>PARABOLAM</span>
+      </p>
     </div>
   );
 }
