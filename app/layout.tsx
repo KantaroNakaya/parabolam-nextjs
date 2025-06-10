74045e389ba2dd53fcba38c32c28932a1e23afd9
KantaroNakaya <shimeno38saiko@gmail.com>
2025-06-10 14:43:36 +0900

    Googleフォントを適用

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,58 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import { Kaisei_HarunoUmi, Noto_Serif_JP } from "next/font/google";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
 import { GoogleTagManager } from "@next/third-parties/google";
 import FixedNav from "./_components/FixedNav";
 import PageTop from "./_components/PageTop";
+
+const kaiseiHarunoUmi = Kaisei_HarunoUmi({
+    weight: "400",
+    subsets: ["latin"],
+    display: "swap",
+});
+
+const notoSerifJP = Noto_Serif_JP({
+    weight: ["400", "700"],
+    subsets: ["latin"],
+    display: "swap",
+});
+
 export const metadata: Metadata = {
-  metadataBase: new URL("https://parabolam-nextjs.vercel.app/"),
-  title: {
-    template: "%s | 架空のバー「PARABOLAM」のデモサイト",
-    default: "架空のバー「PARABOLAM」のデモサイト",
-  },
-  description:
-    "本サイトは架空のバーのデモサイトです。デモサイトのため、noindexにしています。",
-  openGraph: {
-    title: `%s | 架空のバー「PARABOLAM」のデモサイト`,
+    metadataBase: new URL("https://parabolam-nextjs.vercel.app/"),
+    title: {
+        template: "%s | 架空のバー「PARABOLAM」のデモサイト",
+        default: "架空のバー「PARABOLAM」のデモサイト",
+    },
     description:
-      "本サイトは架空のバーのデモサイトです。デモサイトのため、noindexにしています。",
-    images: ["/ogp.jpg"],
-  },
-  alternates: {
-    canonical: `https://parabolam-nextjs.vercel.app/`,
-  },
+        "本サイトは架空のバーのデモサイトです。デモサイトのため、noindexにしています。",
+    openGraph: {
+        title: `%s | 架空のバー「PARABOLAM」のデモサイト`,
+        description:
+            "本サイトは架空のバーのデモサイトです。デモサイトのため、noindexにしています。",
+        images: ["/ogp.jpg"],
+    },
+    alternates: {
+        canonical: `https://parabolam-nextjs.vercel.app/`,
+    },
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="ja">
-      <body>
-        <Header />
-        <main>{children}</main>
-        <Footer />
-        <PageTop />
-        <FixedNav />
-      </body>
-      <GoogleTagManager gtmId="GTM-5627SN3F" />
-    </html>
-  );
+    return (
+        <html lang="ja" className={kaiseiHarunoUmi.className}>
+            <body className={notoSerifJP.className}>
+                <Header />
+                <main>{children}</main>
+                <Footer />
+                <PageTop />
+                <FixedNav />
+            </body>
+            <GoogleTagManager gtmId="GTM-5627SN3F" />
+        </html>
+    );
 }
